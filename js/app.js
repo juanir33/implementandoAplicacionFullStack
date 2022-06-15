9241c214af5e5348d46ffce930eb7b42dc5390f4
juanir33 <guaca33@gmail.com>
2022-06-15 13:59:28 -0300

    Creo funcion eliminar santo

--- js/app.js
+++ js/app.js
@@ -1,24 +1,20 @@
-const axiosClient = axios.create({baseURL: 'http://localhost:3000'});
-const selector = (tag)=>{
-    return document.querySelector(tag)
-}
+const axiosClient = axios.create({ baseURL: "http://localhost:3000" });
+const selector = (tag) => {
+  return document.querySelector(tag);
+};
 
+const getAll = async () => {
+  try {
+    const response = await axiosClient.get("/caballeros");
+    const json = await response.data;
+    console.log(json);
+    json.forEach((seiya) => {
+      let row = document.createElement("tr");
 
-
-
-const getAll = async ()=>{
-    try{
-        
-        const response = await fetch("http://localhost:3000/caballeros");
-        const json = await response.json();
-        console.log(json);
-        json.forEach(seiya => {
-            let row = document.createElement("tr");
-            
-            row.innerHTML = `           
+      row.innerHTML = `           
             <td class="col-sm-2 col-md-2 col-lg-1">${seiya.nombre}</td>
             <td class="col-sm-3 col-md-3 col-lg-1">${seiya.constelacion}</td>
-            <td class="col-sm-5 col-md-5 col-lg-3">${(seiya.armadura)}</td>
+            <td class="col-sm-5 col-md-5 col-lg-3">${seiya.armadura}</td>
             
             <td class="col-sm-2 col-md-2 col-lg-1">
             <section class="d-flex  justify-content-center align-items-center btnOpciones">
@@ -31,80 +27,86 @@ const getAll = async ()=>{
             
             </section>
             
-            `
-            row.className="paddingTr tablaColorText";
-            const table = document.querySelector("#cuerpoTabla");
-            table.append(row);
-           
-            
-            
-        });
-    }catch(err){
-        console.log(err);
-    }
-     
-
+            `;
+      row.className = "paddingTr tablaColorText";
+      const table = document.querySelector("#cuerpoTabla");
+      table.append(row);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const agregarCaballero = async (event) => {
-    event.preventDefault();
-    let name = selector("#seiyaName").value;
-    let constel = selector("#seiyaConstelacion").value;
-    let armadura = selector("#seiyaArmadura").value;
-    let id = selector("#seiyaId").value;
-    let bodyData = {
-        "nombre": name,
-        "constelacion": constel,
-        "armadura": armadura,
+  event.preventDefault();
+  let name = selector("#seiyaName").value;
+  let constel = selector("#seiyaConstelacion").value;
+  let armadura = selector("#seiyaArmadura").value;
+  let id = selector("#seiyaId").value;
+  let bodyData = {
+    nombre: name,
+    constelacion: constel,
+    armadura: armadura,
+  };
+  try {
+    if (!id) {
+      let { data } = await axiosClient.post("/caballeros", bodyData);
+    } else {
+      let { data } = await axiosClient.put(`/caballeros/${id}`, bodyData);
     }
-    try {
-        if(!id){
-        let {data} = await axiosClient.post('/caballeros', bodyData);
-         
-    }else { let {data} = await axiosClient.put(`/caballeros/${id}`, bodyData)}
-    } catch (error) {
-        console.log(error);
-    }
-    
-    
-    
-    
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+const editarDatosSeiya = async (id) => {
+  try {
+    let response = await axiosClient.get("/caballeros");
+    let seiyas = await response.data;
+    seiyas.forEach((seiya) => {
+      if (seiya.id === id) {
+        selector("#seiyaName").value = seiya.nombre;
+        selector("#seiyaConstelacion").value = seiya.constelacion;
+        selector("#seiyaArmadura").value = seiya.armadura;
+        selector("#seiyaId").value = seiya.id;
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-
-const editarDatosSeiya = async (id)=>{
-    try {
-        let  response = await axiosClient.get('/caballeros');
-        let seiyas = await response.data;
-        seiyas.forEach(seiya => {
-        if(seiya.id === id) {
-             selector("#seiyaName").value = seiya.nombre;
-             selector("#seiyaConstelacion").value= seiya.constelacion;
-             selector("#seiyaArmadura").value = seiya.armadura;
-             selector("#seiyaId").value = seiya.id;
-         
-          
-        }}
-    )
-    } catch (error) {
-        console.log(error);
+const eliminarSeiya = (id) => {
+  try {
+    Swal.fire({
+      title: "¿Eliminar Caballero?",
+      showDenyButton: false,
+      showCancelButton: true,
+      confirmButtonText: "Confirmar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Santo eliminado',
+            showConfirmButton: true,
+            
+            }).then(async (results) => {if (results.isConfirmed)await axiosClient.delete(`/caballeros/${id}`);})
+            
         
-    }
+       }})}
+      
     
-    
-    
+        
+  catch (error) {
+    console.log(error);
+  }
+};
 
+function limpiarCamposModal() {
+  selector("#seiyaName").value = "";
+  selector("#seiyaConstelacion").value = "";
+  selector("#seiyaArmadura").value = "";
 }
-function limpiarCamposModal (){
-    
-      selector("#seiyaName").value = "";
-      selector("#seiyaConstelacion").value = "";
-      selector("#seiyaArmadura").value = "";
-    
-}
 
-
-
-
-document.addEventListener("DOMContentLoaded",getAll)
+document.addEventListener("DOMContentLoaded", getAll);
